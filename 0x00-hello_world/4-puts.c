c556fd7167dfd9b9abf9db5f6f392ca71eeef239
MbuviM <mwendembuvi10@gmail.com>
2023-08-10 16:59:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: Prints a message followed by a new line
- * using the `puts` function.
+ * Description: Prints a specific text followed by a new line
+ *              using the `puts` function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts('\"Programming is like building a multilingual puzzle');
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+

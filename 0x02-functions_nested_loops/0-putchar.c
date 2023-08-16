bac48b1798cbaaba0b0bfdee1ec52e2ab8eeccf6
MbuviM <mwendembuvi10@gmail.com>
2023-08-16 06:55:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - Entry point of the program
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	putchar('_');
 	putchar('p');
@@ -15,5 +15,5 @@ int main (void)
 	putchar('a');
 	putchar('r');
 	putchar('\n');
-return(0);
+return (0);
 }

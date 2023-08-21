e921da1f9901deddb21517597f80485ad2b50199
MbuviM <mwendembuvi10@gmail.com>
2023-08-21 10:49:03 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,10 +14,12 @@ void rev_string(char *s)
 
 	len = strlen(s);
 
-	for (letter = len - 1; letter < length/2; letter++)
+	for (letter = len - 1; letter < len / 2; letter++)
 	{
-		char temp = [letter];
-		s[letter] = s[length - letter - 1];
+		char temp;
+
+		temp = s[letter];
+		s[letter] = s[len - letter - 1];
 		s[letter] = temp;
 	}
 }

731e9da6e351dbc4f43d710fd494a7a429d21fbb
MbuviM <mwendembuvi10@gmail.com>
2023-08-21 07:29:03 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Functions that prints the string
+ * @str:  pointer parameter variable.
+ * Return: (0) Success.
+*/
+
+void _puts(char *str)
+{
+	puts(*str);
+	return (0);
+}

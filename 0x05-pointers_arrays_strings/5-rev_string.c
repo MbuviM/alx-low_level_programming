a46c17e6193f73f31984a65cafa465fbcb5c6668
MbuviM <mwendembuvi10@gmail.com>
2023-08-21 10:59:13 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,12 +14,21 @@ void rev_string(char *s)
 
 	len = strlen(s);
 
-	for (letter = len - 1; letter < len / 2; letter++)
+	for (letter = 0; letter < len / 2; letter++)
 	{
 		char temp;
 
 		temp = s[letter];
 		s[letter] = s[len - letter - 1];
-		s[letter] = temp;
+		s[len - letter - 1] = temp;
 	}
+}
+int main(void)
+{
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
 }

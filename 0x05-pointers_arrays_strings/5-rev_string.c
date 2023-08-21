663138fdb7fa2f543b920dc472283e66de2cec76
MbuviM <mwendembuvi10@gmail.com>
2023-08-21 10:38:57 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 #include <stdio.h>
 
 /**
@@ -14,8 +14,10 @@ void rev_string(char *s)
 
 	len = strlen(s);
 
-	for (letter = len - 1; letter >= 0; letter--)
+	for (letter = len - 1; letter < length/2; letter++)
 	{
-		putchar(s[letter]);
+		char temp = [letter];
+		s[letter] = s[length - letter - 1];
+		s[letter] = temp;
 	}
 }

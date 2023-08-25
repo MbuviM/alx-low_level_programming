1e70f763843cbb2cc6b889e06292f2cbdd7cb86d
MbuviM <mwendembuvi10@gmail.com>
2023-08-25 10:17:08 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *leet(char *);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
 
 #endif

2e71d1ff5c4733c1193369a8d8b671c23f8dbc5e
MbuviM <mwendembuvi10@gmail.com>
2023-08-25 05:07:07 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strncat(char *dest, char *src, int n);
 char *rot13(char *);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 
 #endif
